3ece402e1077648f0e7d0404bda353dfacc46a14
Dilruba Khan <rubatanisa@yahoo.ie>
2021-10-17 15:19:50 +0100

    finish porject

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function msg(){  
-    console.log('Hello!!!');
-   }  
+const text = document.querySelector('.message');
+const submitButton = document.querySelector('.btn');
 
-msg();
+
+
+const getMessage = async()=>{
+    const res = await fetch(`http://localhost:3000/messages`);
+    const messages = await res.json();
+    
+    const num = Math.floor(Math.random()*messages.length); 
+    const item = messages[num];
+    
+    const message = item.message;   
+    text.innerText = message;
+
+    console.log(message);   
+}
+submitButton.addEventListener('click', getMessage);
+
+getMessage();
+
